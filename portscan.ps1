95b1f3422ca43d77685e11b8c2058ba2e1fea125
returaxel <48722741+returaxel@users.noreply.github.com>
2024-02-19 20:56:49 +0100

    update

--- portscan.ps1
+++ portscan.ps1
@@ -1,11 +1,7 @@
-$CSVSrc = Import-CSV ".\portscan_server.csv" 
+$CSVSrc = Import-CSV ".\portscan_server.csv"
 
 # Path
 $SavePath = "C:\Temp\PortScan"
-
-# INF
-Write-Host "Getting status of the following hosts and ports"
-Write-Host $row -ForegroundColor DarkGray
 
 # Current Host
 $TargetHost = $null
@@ -13,70 +9,63 @@ $TargetAddr = $null
 
 # Scriptblock
 $SB_ICMP = { 
-    param($TargetHost,$TargetAddr) 
+    param($TargetHost,$TargetAddr,$SavePath) 
     $ProgressPreference = 'SilentlyContinue'
+
     if (tnc -ComputerName $TargetAddr -TraceRoute -InformationLevel Quiet) {
-        tnc -ComputerName $TargetAddr -TraceRoute -InformationLevel Detailed | Out-File "$SavePath\$($TargetHost),ICMP,TRUE.txt"
-        Write-Output "ICMP,`t`t TRUE, $TargetHost($TargetAddr)"
+        #tnc -ComputerName $TargetAddr -TraceRoute -InformationLevel Detailed | 
+        Out-File "$SavePath\$($TargetHost),ICMP,TRUE.txt" -Force
+        Write-Output "$(Get-Date -Format HH:mm:ss.fff), ICMP,`t TRUE, $TargetHost, $TargetAddr"
     }
     else {
-        try {
-            Out-File "$SavePath\$($TargetHost),ICMP,FALSE.log"
-            Write-Output "ICMP,`t`tFALSE, $TargetHost($TargetAddr)"
-        }
-        catch {
-        }
+        Out-File "$SavePath\$($TargetHost),ICMP,FALSE.log"
+        Write-Output "$(Get-Date -Format HH:mm:ss.fff),ICMP,`t FALSE, $TargetHost, $TargetAddr"
         }
     }
 
 $SB_PORT = { 
-    param($TargetHost,$TargetAddr,$port
-    ) 
+    param($TargetHost,$TargetAddr,$SavePath,$port) 
     $ProgressPreference = 'SilentlyContinue'
 
     if (tnc -ComputerName $TargetAddr -TraceRoute -InformationLevel Quiet) {
-        if (tnc -ComputerName $TargetAddr -Port $port -InformationLevel Quiet) {
-            Out-File "$SavePath\$($TargetHost),$($port),TRUE.txt" ; Write-Output "Port $($port),`t TRUE, $TargetHost($TargetAddr)"
+
+        #  Initialize object
+        $PortWhack = New-Object -TypeName Net.Sockets.TcpClient
+        if (($PortWhack.BeginConnect($TargetAddr,[int]$port,$Null,$Null)).AsyncWaitHandle.WaitOne(2500)) {
+            Out-File "$SavePath\$($TargetHost),$($port),TRUE.txt" -Force
+            Write-Output "$(Get-Date -Format HH:mm:ss.fff), $($port),`t TRUE, $TargetHost, $TargetAddr"
         }
         else {
-            try {
-                Out-File "$SavePath\$($TargetHost),$($port),FALSE.log" ; Write-Output "Port $($port),`T FALSE, $TargetHost($TargetAddr)"
-            }
-            catch {
-            }
+            Out-File "$SavePath\$($TargetHost),$($port),FALSE.log" -Force
+            Write-Output "$(Get-Date -Format HH:mm:ss.fff), $($port),`t FALSE, $TargetHost, $TargetAddr"
         }
     }
+    $PortWhack.Close()
 }
-    
-
-Write-Host "Testing... ICMP"
 
 # Iterate 
 foreach ($row in $CSVSrc) {
-        Write-Host "`n[info] Target: " -NoNewline -ForegroundColor Cyan
-        $TargetHost = $row.Host
-        $TargetAddr = $row.IP
-        Write-Host "$TargetHost, $TargetAddr"
+    Write-Host "[info] Target: " -NoNewline -ForegroundColor Cyan
+    $TargetHost = $row.Host
+    $TargetAddr = $row.IP
+    Write-Host $row -ForegroundColor DarkGray
 
-        Start-Job -Name "Trace_$($row.Description)" -ScriptBlock $SB_ICMP -ArgumentList @($TargetAddr, $TargetHost) | Out-Null
+    Start-Job -Name "Trace_$($row.Description)" -ScriptBlock $SB_ICMP -ArgumentList @($TargetAddr, $TargetHost ,$SavePath) | Out-Null
 
-        $ports = $row.Ports -split " "
-        foreach ($port in $ports) {
-            Start-Job -Name "Prt_$($row.Description)" -ScriptBlock $SB_PORT -ArgumentList @($TargetAddr, $TargetHost, $port) | Out-Null
-        }
+    $ports = $row.Ports -split " "
+    foreach ($port in $ports) {
+        Start-Job -Name "Prt_$($row.Description)" -ScriptBlock $SB_PORT -ArgumentList @($TargetAddr, $TargetHost,$SavePath, [int]$port) | Out-Null
+    }
+}
 
-    # Avoid mistakes
-    Clear-Variable TargetHost, TargetAddr
-
-    # Wait for jobs to finish
-    $StillRunning = $true
-    Write-Host "`nResults:" -ForegroundColor Blue
-    while ($StillRunning) {
-        Start-Sleep 3
-        if ((Get-Job).State -notcontains "Running") {
-            Receive-Job *
-            $StillRunning = $false
-            Remove-Job * 
-        }
+# Wait for jobs to finish
+# Move inside loop to get grouped results for each host
+$StillRunning = $true
+while ($StillRunning) {
+    Start-Sleep 5
+    if ((Get-Job).State -notcontains "Running") {
+        Receive-Job *
+        $StillRunning = $false
+        Remove-Job * 
     }
 }
